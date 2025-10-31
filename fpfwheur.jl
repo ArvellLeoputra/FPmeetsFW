ef9c4b3e440fc5c4182369ec9169b933f2e2dc01
ArvellLeoputra <arvell.leo@gmail.com>
2025-10-31 01:27:59 +0100

    Update find_primal_solution function
    The find_primal_solution function is updated to retrieve the bounds of all variables and to print it out, additionally changed the example LP to neos5.mps

--- fpfwheur.jl
+++ fpfwheur.jl
@@ -1,4 +1,6 @@
 using SCIP
+using JuMP
+using FrankWolfe
 
 mutable struct FPFWHeuristic <: SCIP.Heuristic
     max_iter::Int
@@ -9,12 +11,51 @@ function SCIP.find_primal_solution(
     heur::FPFWHeuristic,
     heurtiming::SCIP.SCIP_HEURTIMING,
     nodeinfeasible::SCIP.Bool,
-    heur_ptr::Ptr{SCIP.SCIP_HEUR}
+    heur_ptr::Ptr{SCIP.SCIP_HEUR},
 )::Tuple{SCIP.SCIP_RETCODE, SCIP.SCIP_RESULT}
 
     @info("FWFP heuristic called")
-    result = SCIP.SCIP_DIDNOTFIND
-    return (SCIP.SCIP_OKAY, result)
+    @assert SCIP.SCIPhasCurrentNodeLP(scip) == SCIP.TRUE  # always true, since we set timing to DURINGLPLOOP
+    result = SCIP.SCIP_DIDNOTRUN
+
+    n_vars = SCIP.SCIPgetNLPCols(scip)
+    n_rows = SCIP.SCIPgetNLPRows(scip)
+    @info "There are $n_vars variables and $n_rows rows in the current LP."
+
+    # For each column, there is a single SCIP_COL object, while for each row there is a single SCIP_ROW object.
+    # First, we get all the SCIP_ROW objects
+    # We have an object called SCIP_ROW, this lies in the heap
+    # So to keep track of the object we have a pointer to the object SCIP_ROW*
+    # Now we an array of such pointers, one for each row so we have SCIP_ROW** (pointer to pointer)
+    ptr_rows = SCIP.SCIPgetLPRows(scip) # SCIP_ROW** // an array of pointers
+    row_pointers = unsafe_wrap(Vector{Ptr{SCIP.SCIP_ROW}}, ptr_rows, n_rows)
+    # row_pointers is an array of pointers to SCIP_ROW objects
+
+    # gets all constraints printed out
+    for (i, row_ptr) in enumerate(row_pointers)
+        # gets the LHS and RHS of the row, i.e., the constraint bounds
+        rhs = SCIP.SCIProwGetRhs(row_ptr)
+        lhs = SCIP.SCIProwGetLhs(row_ptr)
+
+        shift = SCIP.SCIProwGetConstant(row_ptr)
+        nnonz = SCIP.SCIProwGetNNonz(row_ptr)
+
+        non_zero_cols = SCIP.SCIProwGetCols(row_ptr) # SCIP_COL**
+        non_zero_cols_ptrs = unsafe_wrap(Vector{Ptr{SCIP.SCIP_COL}}, non_zero_cols, nnonz)
+        non_zero_vals = SCIP.SCIProwGetVals(row_ptr) # double*
+        non_zero_vec = unsafe_wrap(Vector{SCIP.SCIP_Real}, non_zero_vals, nnonz)
+
+        print("$lhs <=")
+        for j in 1:nnonz
+            var = SCIP.SCIPcolGetVar(non_zero_cols_ptrs[j])
+            name = unsafe_string(SCIP.SCIPvarGetName(var))
+            coeff = non_zero_vec[j]
+            print(" $coeff*$name ")
+        end
+        print(" + $shift <=  $rhs\n")
+    end
+
+    return (SCIP.SCIP_OKAY, SCIP.SCIP_DIDNOTFIND)
 end
 
 optimizer = SCIP.Optimizer()
@@ -22,14 +63,15 @@ optimizer = SCIP.Optimizer()
 heuristic_storage = Dict{Any, Ptr{SCIP.SCIP_HEUR}}()
 
 SCIP.include_heuristic(
-    optimizer,
-    FPFWHeuristic(100),
+    optimizer, 
+    FPFWHeuristic(100); 
     name="FPFWHeuristic",
-    description="Frank–Wolfe Feasibility Pump heuristic",
-    dispchar='F',
-    timing_mask = SCIP.SCIP_HEURTIMING_DURINGLPLOOP
+    description="Frank–Wolfe Feasibility Pump heuristic", 
+    priority=100000, 
+    frequency=0, 
+    timing_mask=SCIP.SCIP_HEURTIMING_DURINGLPLOOP
 )
 
 scip = optimizer.inner
-SCIP.@SCIP_CALL(SCIP.SCIPreadProb(scip, "gen-ip002.mps", C_NULL))
+SCIP.@SCIP_CALL(SCIP.SCIPreadProb(scip, "neos5.mps", C_NULL))
 SCIP.@SCIP_CALL(SCIP.SCIPsolve(scip))
