258859ca3e26151c3d1d1bf8e64791c6a8a34b4b
ArvellLeoputra <arvell.leo@gmail.com>
2026-07-08 14:06:40 +0200

    Update main.jl

--- main.jl
+++ main.jl
@@ -4,9 +4,13 @@ include("scip_setup.jl")
 include("helper.jl")
 include("lmo_builder.jl")
 include("fw_utils.jl")
-include("plot_utils.jl")
 include("fpfwheur.jl")
 include("runner.jl")
 
 fileName, config = loadConfig(ARGS)
+
+if config.enablePlot
+    include("plot_utils.jl")
+end
+
 runInstance(fileName, config)
